c9a1d7cb869ba4ab0b99ce9ee3c8aac4cce10d41
Yann Neves <yannvr@hotmail.com>
2021-11-22 12:56:12 -0300

    add store

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -2,12 +2,25 @@ import { Request, Response } from 'express';
 import { Todo } from '../models/Todo';
 
 export const getAll = async (req: Request, res: Response) => {
+
     const list = await Todo.findAll();
     res.json({ list });
+
 }
 
-export const store = async () => {
-    
+export const store = async (req: Request, res: Response) => {
+
+    if (req.body.title) {
+        
+        let newTodo = await Todo.create({
+            title: req.body.title,
+            done: req.body.done ? true : false
+        });
+
+        res.status(201).json({ item: newTodo });
+    }
+
+    res.status(400).json({ error: 'Dados não enviados.' });
 }
 
 export const update = async () => {
