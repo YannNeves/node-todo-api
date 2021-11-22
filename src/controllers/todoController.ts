95d9c4eb5d934f8aa810964a63347d386309ecaa
Yann Neves <yannvr@hotmail.com>
2021-11-22 12:41:52 -0300

    add todo getAll

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { Todo } from '../models/Todo';
 
-export const getAll = async () => {
-
+export const getAll = async (req: Request, res: Response) => {
+    const list = await Todo.findAll();
+    res.json({ list });
 }
 
 export const store = async () => {
